0f8c026f38dc47cf74de85fb3d892b6d2d8b5c94
ZHKO1 <zhko11993@gmail.com>
2015-10-01 15:08:00 +0800

    2.8 completed

---
+++ ch02/2.1/2.8.scm
@@ -0,0 +1,7 @@
+(load "SICP/ch02/2.1/2.7.scm")
+
+(define (sub-interval x y)
+  (make-interval  (- (lower-bound x) (upper-bound y))
+                  (- (upper-bound x) (lower-bound y))))
+
+(sub-interval x y)
---
+++ ch02/2.1/2.9.scm
@@ -0,0 +1,6 @@
+(load "SICP/ch02/2.1/2.7.scm")
+(load "SICP/ch02/2.1/2.8.scm")
+
+(define (get-width x)
+  (- (cdr x) (car x)))
+
