d944f65fcd996b3073d2be33afaf26c3b3968df8
ZHKO1 <zhko11993@gmail.com>
2015-10-02 23:02:45 +0800

    2.20 completed

---
+++ ch02/2.2/2.20.scm
@@ -0,0 +1,35 @@
+(define (f x y . z)
+  (display x)
+  (newline)
+  (display y)
+  (newline)
+  (display z)
+  (newline)
+  )
+
+(define (g . w)
+  (display w)
+  (newline)
+  )
+
+(f 1 2 3 4 5 6)
+
+(g 1 2 3 4 5 6)
+
+;这里我没有办法优雅的处理cons numb1 (cons numb2 (cons number3 ()
+;最后的null我完全不知道该怎么写
+(define (same-parity . x)
+  (define (out-list items odd?)
+    (if (null? items)
+      (cdr (list 1))
+      (let ((item  (car items)))
+        (cond ((= (remainder item 2) odd?)
+                (cons item (out-list (cdr items) odd?)))
+               (else (out-list (cdr items) odd?))))))
+
+  (if (= (remainder (car x) 2) 1)
+      (out-list x 1)
+      (out-list x 0))
+  )
+
+(same-parity 2 2 3 4 5 6 7 8 9 0 10 11)
