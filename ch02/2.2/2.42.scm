4110041ac8eac871f003d2cf9a6d7aa9740f7aa0
ZHKO1 <zhko11993@gmail.com>
2015-10-16 11:12:38 +0800

    add 2.42 思路

--- ch02/2.2/2.42.scm
+++ ch02/2.2/2.42.scm
@@ -1,6 +1,12 @@
 (load "SICP/ch02/2.2/ex_2.2.3.scm")
 
-
+;这道题比较难 昨天下午一直想到现在
+;我简单介绍下我的思路
+;首先(queens-cols k)的意思就是包含着前k列的几种布局方式
+;(list 布局1 布局2 布局3 .... 布局n)
+;然后我这里 每个布局都是以(list x1 x2 x3 ... xn)的方式来表达
+;xn的含义就是在第n列位于第xn行有个皇后
+;定义清楚了 后面也就好办多了
 
 (define (adjoin-position row col rest-of-queens)
   (append rest-of-queens (list row))
