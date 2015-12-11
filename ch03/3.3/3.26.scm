2d5d9fac3f4af7e1acff8d4065f6ad91a75f7078
ZHKO1 <zhko11993@gmail.com>
2015-12-11 11:02:31 +0800

    3.27 completed

---
+++ ch03/3.3/3.26.scm
@@ -0,0 +1,61 @@
+;差不多就是再次重现树的构成吧
+;感觉蛮有难度的，我都特么忘了一个月前学过的内容
+
+
+;这里还是先简单给出一个实现方式
+(define (make-tree entry left right)
+  (list entry left right))
+
+(define (entry tree)
+  (car tree))
+
+(define (left-branch tree)
+  (cadr tree))
+
+(define (right-branch tree)
+  (caddr tree))
+
+(define (adjoin-set x set)
+  (cond ((null? set) (make-tree x '() '()))
+        ((> (car x) (car (entry set)))
+          (make-tree (entry set)
+                     (left-branch set)
+                     (adjoin-set x (right-branch set))))
+        ((= (car x) (car (entry set))) (set-car! set x))
+        ((< (car x) (car (entry set)))
+          (make-tree (entry set)
+                     (adjoin-set x (left-branch set))
+                     (right-branch set)))
+      ))
+
+(define (make-table)
+  (let ((local-table '()))
+    (define (lookup key records)
+      (cond ((null? records) #f)
+            ((> key (car (entry records)))
+              (lookup key (right-branch records)))
+            ((= key (car (entry records)))
+              (entry records))
+            ((< key (car (entry records)))
+              (lookup key (left-branch records)))            
+          ))
+
+    (define (insert! key value)
+      (let ((record (lookup key local-table)))
+        (if record
+            (set-cdr! record value)
+            (set! local-table (adjoin-set (cons key value) local-table)))))
+
+    (define (get key)
+      (lookup key local-table))
+
+    (define (dispatch m)
+      (cond ((eq? m 'get-proc) get)
+            ((eq? m 'insert-proc) insert!)
+            ((eq? m 'print) local-table)
+            (else (error "Undefined operation -- TABLE", m))
+          ))
+    dispatch))
+(define table (make-table))
+(define get (table 'get-proc))
+(define put (table 'insert-proc))
